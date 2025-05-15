787807a982130df0191825494be64430305d480d
diodemusic <diodemusicuk@gmail.com>
2025-05-15 23:57:41 +0100

    Fix listen callback

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get("/", (req, res) => {
     res.send("Hello world from express and node!");
 })
 
-app.listen(PORT => {
-    console.log(`It's alive! https://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`It's alive! localhost:${PORT}`);
 });
