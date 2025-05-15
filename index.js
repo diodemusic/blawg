cf87c2a029348ff605d953c652ea11afe8947203
diodemusic <diodemusicuk@gmail.com>
2025-05-15 18:12:13 +0100

    Connect to database

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+
+mongoose.connect(MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+    console.error("Connection error:", error);
+});
+
+db.once("open", () => {
+    console.log("Connected to MongoDB");
+});
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on pot ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
