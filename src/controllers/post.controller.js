80e31a926ed04819fddfb53ed9ad587d46c4f1e7
diodemusic <diodemusicuk@gmail.com>
2025-05-17 17:34:05 +0100

    Add createPost function to handle post creation

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,20 @@
+const Post = require("../models/post.model");
+
+async function createPost(req, res) {
+    try {
+        const post = new Post(req.body);
+
+        await post.save();
+
+        res.status(200).send({
+            message: "Post created.",
+            post: post
+        });
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+}
+
+module.exports = {
+    createPost
+};
