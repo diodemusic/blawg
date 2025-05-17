2f6e730e63e82e07be7e12c08bbedefeb311cb6a
diodemusic <diodemusicuk@gmail.com>
2025-05-17 18:30:06 +0100

    Refactor readPosts function to return posts in an object and export readPost function

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -19,7 +19,17 @@ async function readPosts(req, res) {
     try {
         const posts = await Post.find();
 
-        res.status(200).send(posts);
+        res.status(200).send({ posts });
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    }
+}
+
+async function readPost(req, res) {
+    try {
+        const post = await Post.find({ _id: req.params.id });
+
+        res.status(200).send({ post });
     } catch (error) {
         res.status(400).send({ message: error.message });
     }
@@ -27,5 +37,6 @@ async function readPosts(req, res) {
 
 module.exports = {
     createPost,
-    readPosts
+    readPosts,
+    readPost
 };
