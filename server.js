30409f8caed78c0438ae6c39887ee714d399f4bd
diodemusic <diodemusicuk@gmail.com>
2025-05-17 17:33:56 +0100

    Add mongoose and morgan

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 require("dotenv").config();
+const PORT = process.env.PORT;
+const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
 const express = require("express");
 const app = express();
 const health = require("./src/routes/health.route");
+const createPost = require("./src/routes/post.route")
+const mongoose = require("mongoose");
+var morgan = require("morgan");
+
+mongoose.connect(MONGODB_CONNECTION_STRING)
+    .then(() => console.log("connected to mongodb"));
 
 // middleware
 app.use(express.json());
+app.use(morgan("dev"));
 
 // mount routers
 app.use("/api/health/", health);
+app.use("/api/post/", createPost)
 
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`It's alive! localhost:${PORT}`);
 });
