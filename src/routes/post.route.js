011bfe5e0f3d962d9f4e3708d7eaac0a7e48be19
diodemusic <diodemusicuk@gmail.com>
2025-05-17 23:27:15 +0100

    Add deletePost route to enable post deletion functionality

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, readPosts, readPost } = require("../controllers/post.controller");
+const { createPost, readPosts, readPost, deletePost } = require("../controllers/post.controller");
 
 router.post("/create/", createPost);
 router.get("/read-all/", readPosts);
 router.get("/read/:id/", readPost);
+router.delete("/delete/:id/", deletePost);
 
 module.exports = router;
