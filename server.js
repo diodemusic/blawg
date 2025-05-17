c77b266f4741460d94f7d7c9bcc7aa861093ae4c
diodemusic <diodemusicuk@gmail.com>
2025-05-17 23:26:52 +0100

    Reorganize server.js for improved readability by grouping related sections

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
+// enviroment variables
 require("dotenv").config();
 const PORT = process.env.PORT;
 const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
+
+// set up express app
 const express = require("express");
 const app = express();
+
+// require routes
 const healthRoutes = require("./src/routes/health.route");
 const postRoutes = require("./src/routes/post.route")
+
+// require mongoose and connect
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 
 mongoose.connect(MONGODB_CONNECTION_STRING)
     .then(() => console.log("connected to mongodb"));
+
+// require logger
+const morgan = require("morgan");
 
 // middleware
 app.use(express.json());
@@ -19,6 +28,7 @@ app.use(morgan("dev"));
 app.use("/api/health/", healthRoutes);
 app.use("/api/post/", postRoutes);
 
+// start listening
 app.listen(PORT, () => {
     console.log(`It's alive! localhost:${PORT}`);
 });
