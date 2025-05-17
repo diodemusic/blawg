9959bd2e87baa77870d245043f3e30c6cce22248
diodemusic <diodemusicuk@gmail.com>
2025-05-17 18:30:12 +0100

    Add readPost route to retrieve a single post by ID

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, readPosts } = require("../controllers/post.controller");
+const { createPost, readPosts, readPost } = require("../controllers/post.controller");
 
 router.post("/create/", createPost);
 router.get("/read-all/", readPosts);
+router.get("/read/:id/", readPost);
 
 module.exports = router;
