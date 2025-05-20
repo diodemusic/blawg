54fbda76aa1fb90911992abe97331aa06de2abee
diodemusic <diodemusicuk@gmail.com>
2025-05-20 01:23:47 +0100

    Refactor post controller responses for consistency and clarity

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -6,10 +6,7 @@ async function createPost(req, res) {
 
         await post.save();
 
-        res.status(200).send({
-            message: "Post created.",
-            post: post
-        });
+        res.status(200).send({ post });
     } catch (error) {
         res.status(400).send({ message: error.message });
     }
@@ -35,14 +32,22 @@ async function readPost(req, res) {
     }
 }
 
+async function updatePost(req, res) {
+    try {
+        const post = await Post.findOneAndUpdate({ _id: req.params.id }, req.body);
+        const updatePost = await Post.find({ _id: post._id });
+
+        res.status(200).send({ updatePost });
+    } catch (error) {
+        res.status(400).send({ message: error.message });
+    };
+}
+
 async function deletePost(req, res) {
     try {
         const post = await Post.deleteOne({ _id: req.params.id });
 
-        res.status(200).send({
-            message: "Post deleted",
-            documentsDeletedCount: post.deletedCount
-        });
+        res.status(200).send({ post });
     } catch (error) {
         res.status(400).send({ message: error.message });
     }
@@ -52,5 +57,6 @@ module.exports = {
     createPost,
     readPosts,
     readPost,
+    updatePost,
     deletePost
 };
