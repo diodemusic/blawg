c04a58ca824ebae54922afbeed0133b0cb506a4f
diodemusic <diodemusicuk@gmail.com>
2025-05-20 01:23:59 +0100

    Add updatePost route to post router for post updates

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, readPosts, readPost, deletePost } = require("../controllers/post.controller");
+const { createPost, readPosts, readPost, updatePost, deletePost } = require("../controllers/post.controller");
 
 router.post("/create/", createPost);
 router.get("/read-all/", readPosts);
 router.get("/read/:id/", readPost);
+router.put("/update/:id", updatePost);
 router.delete("/delete/:id/", deletePost);
 
 module.exports = router;
