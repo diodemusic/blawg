95931709c1f6dc9f5214b09a73873bd3ed2b74e4
diodemusic <diodemusicuk@gmail.com>
2025-05-22 19:16:23 +0100

    Add PostList component to fetch and display posts from API

---
+++ frontend/src/components/PostList.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function PostList() {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/post/read-all/")
+            .then(response => {
+                setPosts(response.data.posts);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error(`Error fetching post: ${error}`);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return <p>Loading...</p>
+    }
+
+    posts.map(post => {
+        console.log(post);
+    });
+
+    return (
+        <div>
+            {posts.map(post => (
+                <>
+                    <h2 key={post._id}>{post.title}</h2>
+                </>
+            ))}
+        </div>
+    );
+};
